c42f5f81d1f7d26562e3d0bdf838c7abe539d404
Janik Knittle <janik.knittle@gmail.com>
2015-05-12 20:17:09 -0600

    Added CRUD to vehicles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511230511) do
+ActiveRecord::Schema.define(version: 20150513005935) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,5 +30,13 @@ ActiveRecord::Schema.define(version: 20150511230511) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "vehicles", force: :cascade do |t|
+    t.integer  "year"
+    t.string   "make"
+    t.string   "model"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
