66cb89d3a0b4344a95e99465dbafc038c359f34d
Janik Knittle <janik.knittle@gmail.com>
2015-06-23 00:49:07 -0700

    Added Username to User, User Validations and User tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :parts
-  validates :email, uniqueness: true
+  validates :username, uniqueness: true, presence: true, length: { maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255},
+                    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150605050438) do
+ActiveRecord::Schema.define(version: 20150623064232) do
 
   create_table "parts", force: :cascade do |t|
     t.string   "name"
@@ -46,10 +46,12 @@ ActiveRecord::Schema.define(version: 20150605050438) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "vehicles", force: :cascade do |t|
     t.integer  "year"
