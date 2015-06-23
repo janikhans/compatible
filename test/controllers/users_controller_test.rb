05bf66de6084f0e727276d95db2e0d35dbb8c3df
Janik Knittle <janik.knittle@gmail.com>
2015-06-22 23:41:34 -0700

    Added dummy content to Contact, Help, About, Blog pages

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+	def setup 
+		@user = User.new(name: "Example User", email: "user@example.com")
+	end
+
+	test "should be valid" do
+		assert_response @user.valid?
+	end
   
 end
