b8fc19a9ac734be9669e6f1141c7d608139c439c
Janik Knittle <janik.knittle@gmail.com>
2015-08-09 18:42:34 -0700

    Moved parts to prtials and chaned some styling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,15 @@
 class SearchController < ApplicationController
 
 	def index
-		@make = params[:make].upcase
-    	@model = params[:model].upcase
+		@make = params[:make].upcase.strip
+    	@model = params[:model].upcase.strip
     	@year = params[:year]
-    	@part = params[:part]
+    	@part = params[:part].strip
     	@vehicle = Vehicle.where(make: @make, model: @model, year: @year).first
-    	@oem_part = Part.where()
+    	@oem_parts = @vehicle.parts.where(name:@part)
 
     	@discoveries = Discovery.where(oem: @vehicle)
+        # @discoveries = @vehicle.discoveries.where(replaces: include(@part))
 
     	# @vehicle = Vehicle.where(make: @make, model: @model, year: @year).first_or_create
     	# @oemfitment = current_user.oemfitments.build(part: @part, vehicle: @vehicle)
